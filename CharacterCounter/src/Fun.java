ef1d8fe56546d8fecf60a96265bd2bf8bf89edb7
Cole Polyak <35577611+PolyCole@users.noreply.github.com>
2018-10-18 20:28:50 -0600

    Clean Fun.java logic with Character Processing
    Changed the existing switch statement to a modulus operator performing same task.

--- CharacterCounter/src/Fun.java
+++ CharacterCounter/src/Fun.java
@@ -37,116 +37,14 @@ public class Fun {
 
 		//Parsing through file
 		while(fileIn.hasNext()) {
-			String word = fileIn.next();
-			word = word.toLowerCase();
+			String word = fileIn.next().toLowerCase();
 		
 			//Parsing to find values of individual chars
 			char[] wordLetters = word.toCharArray();
-			for (int i = 0; i < word.length(); i++) {
-				switch (wordLetters[i]) {
-					case 'a':
-						count[0]++;
-						break;
-						
-					case 'b':
-						count[1]++;
-						break;
-						
-					case 'c':
-						count[2]++;
-						break;
-						
-					case 'd':	
-						count[3]++;
-						break;
-						
-					case 'e':
-						count[4]++;
-						break;
-						
-					case 'f':
-						count[5]++;
-						break;
-						
-					case 'g':
-						count[6]++;
-						break;
-						
-					case 'h':
-						count[7]++;
-						break;
-						
-					case 'i':
-						count[8]++;
-						break;
-						
-					case 'j':
-						count[9]++;
-						break;
-						
-					case 'k':
-						count[10]++;
-						break;
-						
-					case 'l':
-						count[11]++;
-						break;
-						
-					case 'm':
-						count[12]++;
-						break;
-						
-					case 'n':
-						count[13]++;
-						break;
-						
-					case 'o':
-						count[14]++;
-						break;
-						
-					case 'p':
-						count[15]++;
-						break;
-						
-					case 'q':
-						count[16]++;
-						break;
-						
-					case 'r':
-						count[17]++;
-						break;
-						
-					case 's':
-						count[18]++;
-						break;
-						
-					case 't':
-						count[19]++;
-						break;
-						
-					case 'u':
-						count[20]++;
-						break;
-						
-					case 'v':
-						count[21]++;
-						break;
-						
-					case 'w':
-						count[22]++;
-						break;
-						
-					case 'x':
-						count[23]++;
-						break;
-						
-					case 'y':
-						count[24]++;
-						break;
-						
-					case 'z':
-						count[25]++;
-						break;
+			for(int i = 0; i < word.length(); i++) {
+				if(Character.isAlphabetic(wordLetters[i])) {
+					int current = (int)wordLetters[i];
+					count[current % 97]++;
 				}
 			}
 			
